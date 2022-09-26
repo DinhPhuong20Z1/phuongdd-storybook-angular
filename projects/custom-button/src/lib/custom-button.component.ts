a129d4e0c662a689dba215e39d60a7c723fd63a5
phuong1772001 <phuongdd@volio.vn>
2022-09-26 10:59:58 +0700

    update

--- projects/custom-button/src/lib/custom-button.component.ts
+++ projects/custom-button/src/lib/custom-button.component.ts
@@ -1,21 +1,33 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter  } from '@angular/core';
 
 @Component({
   selector: 'lib-custom-button',
-  template: `
-    <p>
-      <button [style.color]='color' >Hello</button>
-    </p>
-  `,
-  styles: [
-  ]
+   template: ` <button
+   type="button"
+   (click)="onClick.emit($event)"
+   [ngClass]="classes"
+   [style.color]='color'
+ >
+   {{ label }}
+ </button>`,
+  styleUrls: ['./button.css'],
 })
-export class CustomButtonComponent implements OnInit {
-  @Input() color: string = '#000'
+export class CustomButtonComponent  {
+  @Input() color: string = '#000';
+  label = 'Button';
+  @Input()
+  primary = false;
+  @Input()
+  size: 'small' | 'medium' | 'large' = 'medium';
+  @Output()
+  onClick = new EventEmitter<Event>();
 
-  constructor() { }
+  public get classes(): string[] {
+    const mode = this.primary ? 'storybook-button--primary' : 'storybook-button--secondary';
 
-  ngOnInit(): void {
+    return ['storybook-button', `storybook-button--${this.size}`, mode];
   }
+
+
 
 }
--- projects/custom-button/src/lib/custom-button.module.ts
+++ projects/custom-button/src/lib/custom-button.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { CustomButtonComponent } from './custom-button.component';
 
 
@@ -6,11 +7,13 @@ import { CustomButtonComponent } from './custom-button.component';
 @NgModule({
   declarations: [
     CustomButtonComponent
+
   ],
   imports: [
+    BrowserModule
   ],
   exports: [
-    CustomButtonComponent
+
   ]
 })
 export class CustomButtonModule { }
